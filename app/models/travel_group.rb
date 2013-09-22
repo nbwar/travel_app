7024c01e9a9a9d98362f5f40920ec1314439f833
Nicholas Wargnier <nwargnier@gmail.com>
2013-09-21 17:14:26 -0700

    create controllers

--- app/models/travel_group.rb
+++ app/models/travel_group.rb
@@ -1,4 +1,5 @@
 class TravelGroup < ActiveRecord::Base
+  #TODO: Either create a groupe token or invitation to group
   has_and_belongs_to_many :users
   has_many :suggestions
   has_many :excursions, through: :suggestions
